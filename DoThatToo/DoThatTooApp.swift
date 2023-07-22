f0f06c5900d8c35a36119cee44f8c45d67fbc5dd
GregDeveaux <92979399+GregDeveaux@users.noreply.github.com>
2023-07-22 11:56:18 +0200

    First commit

---
+++ DoThatToo/DoThatTooApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoThatTooApp.swift
+//  DoThatToo
+//
+//  Created by Greg-Mini on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DoThatTooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
